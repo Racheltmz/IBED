f546d84a84ae7b153e9d80a89e28101c0e9518a1
Rachel Tan <racheltanmz@gmail.com>
2021-09-29 14:07:38 +0800

    Edited to ensure responsiveness

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+var elem = document.getElementById("intro_video");
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+    Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
