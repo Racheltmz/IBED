9410639f20d50ffa4fadc192fb14a765dee13984
PekYC <91200212+PekYC@users.noreply.github.com>
2021-10-03 12:34:59 +0800

    Added recordings.js

---
+++ js/recordings.js
@@ -0,0 +1,10 @@
+//  File Name: recordings.js
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
