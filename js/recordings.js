371bfa9815c79cf0e917df5effa49270b1ee2c64
PekYC <91200212+PekYC@users.noreply.github.com>
2021-10-04 15:39:11 +0800

    Updated recordings.js

--- js/recordings.js
+++ js/recordings.js
@@ -2,9 +2,11 @@
 
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
+    document.getElementsByClassName("overlay")[0].className += " active";
 }
 
 
 function closeNav() {
     document.getElementById("sidenav").style.width = "0";
+    document.getElementsByClassName("overlay")[0].className = "overlay";
 }
