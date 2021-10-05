164a8914f4f436caf98cec69f46f1b6f4a91173c
PekYC <91200212+PekYC@users.noreply.github.com>
2021-10-05 16:22:54 +0800

    Created livestream.js

---
+++ js/livestream.js
@@ -0,0 +1,78 @@
+// File Name: schedule.js
+
+var sgText = document.getElementsByClassName("time");
+sgTime = [];
+for (i = 0; i < sgText.length; i++) {
+    var text = sgText[i].textContent;
+    text = text.split("-");
+    var result = text.map(function (x) {
+        temp = x.split(":");
+        hour = parseInt(temp[0], 10);
+        hour *= 60;
+        second = parseInt(temp[1], 10);
+        second += hour;
+        return second;
+    });
+    sgTime.push(result);
+}
+
+function convert(time) {
+    text = [];
+    for (i = 0; i < time.length; i++) {
+        var indiv = time[i];
+        var result = indiv.map(function (x) {
+            hours = Math.floor(x / 60);
+            min = x - (hours * 60);
+            string = hours.toString().padStart(2, "0") + ":" + min.toString().padStart(2, "0");
+            return string;
+        });
+        text.push(result);
+    }
+    return text;
+}
+
+function change(time, difference) {
+    newTime = [];
+    for (i = 0; i < time.length; i++) {
+        var diffed = time[i].map(function (x) {
+            return x - difference;
+        });
+        newTime.push(diffed);
+    }
+    return newTime;
+}
+
+// Generating India time and text
+idTime = change(sgTime, 150);
+idText = convert(idTime);
+
+// Generating London time and text
+lnTime = change(sgTime, 420);
+lnText = convert(lnTime);
+
+sgTexter = convert(sgTime);
+
+// Applying functions to respective buttons
+var SG = document.getElementById("SGl");
+SG.onclick = function () {
+    for (i = 0; i < sgText.length; i++) {
+        currentText = sgText[i];
+        currentText.innerHTML = sgTexter[i][0] + "<br>-<br>" + sgTexter[i][1];
+    }
+}
+
+var ID = document.getElementById("IDl");
+ID.onclick = function () {
+    for (i = 0; i < sgText.length; i++) {
+        currentText = sgText[i];
+        currentText.innerHTML = idText[i][0] + "<br>-<br>" + idText[i][1];
+    }
+}
+
+var LN = document.getElementById("LNl");
+LN.onclick = function () {
+    for (i = 0; i < sgText.length; i++) {
+        currentText = sgText[i];
+        currentText.innerHTML = lnText[i][0] + "<br>-<br>" + lnText[i][1];
+    }
+}
