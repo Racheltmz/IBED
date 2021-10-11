8b425d9bef562ba9a24f20602369db07ba9f953f
PekYC <91200212+PekYC@users.noreply.github.com>
2021-10-11 22:00:32 +0800

    Changed schedule and livestream javascript

--- js/schedule.js
+++ js/schedule.js
@@ -20,13 +20,15 @@ function expand(selected) {
         }
 
         if (text.getAttribute('data-collapsed') != 'true') {
-            text.style.maxHeight = "100px";
+            text.style.maxHeight = "150px";
             text.style.transition = "max-height 0.25s ease-out";
+            text.style.marginBottom = "1rem";
             text.setAttribute('data-collapsed', 'true');
             arrow.classList.add('flip');
         } else {
             text.style.maxHeight = "0px";
             text.style.transition = "max-height 0.15s ease-out";
+            text.style.marginBottom = "0";
             text.setAttribute('data-collapsed', 'false');
             arrow.classList.remove('flip');
         }
@@ -72,7 +74,11 @@ function change(time, difference) {
     newTime = [];
     for (i = 0; i < time.length; i++) {
         var diffed = time[i].map(function (x) {
-            return x - difference;
+            changedTime = x - difference;
+            if (changedTime < 0) {
+                changedTime = 1440 + changedTime;
+            }
+            return changedTime;
         });
         newTime.push(diffed);
     }
@@ -87,15 +93,31 @@ idText = convert(idTime);
 lnTime = change(sgTime, 420);
 lnText = convert(lnTime);
 
+usTime = change(sgTime, 840);
+usText = convert(usTime);
+
 sgTexter = convert(sgTime);
 
 // Applying functions to respective buttons
+text1 = "Day 1 - January 15, 2022 (Saturday)";
+text2 = "Day 2 - January 16, 2022 (Sunday)";
+
+function replaceText() {
+    var day1 = document.getElementById("day1");
+    var day2 = document.getElementById("day2");
+    day1.innerHTML = text1;
+    day2.innerHTML = text2;
+}
+
 var SG = document.getElementById("SGl");
 SG.onclick = function () {
     for (i = 0; i < sgText.length; i++) {
         currentText = sgText[i];
         currentText.innerHTML = sgTexter[i][0] + "<br>-<br>" + sgTexter[i][1];
     }
+    var timetext = document.getElementById("timetext");
+    timetext.innerHTML = "(Malaysia)";
+    replaceText();
 }
 
 var ID = document.getElementById("IDl");
@@ -104,6 +126,9 @@ ID.onclick = function () {
         currentText = sgText[i];
         currentText.innerHTML = idText[i][0] + "<br>-<br>" + idText[i][1];
     }
+    var timetext = document.getElementById("timetext");
+    timetext.innerHTML = "(India)";
+    replaceText();
 }
 
 var LN = document.getElementById("LNl");
@@ -112,4 +137,22 @@ LN.onclick = function () {
         currentText = sgText[i];
         currentText.innerHTML = lnText[i][0] + "<br>-<br>" + lnText[i][1];
     }
+    var timetext = document.getElementById("timetext");
+    timetext.innerHTML = "(UK)";
+    replaceText();
 }
+
+var US = document.getElementById("USl");
+US.onclick = function () {
+    for (i = 0; i < sgText.length; i++) {
+        currentText = sgText[i];
+        currentText.innerHTML = usText[i][0] + "<br>-<br>" + usText[i][1];
+    }
+    var timetext = document.getElementById("timetext");
+    timetext.innerHTML = "(US, Colorado)";
+    var day1 = document.getElementById("day1");
+    day1.innerHTML = "Day 1 - January 14-15, 2022 (Fri-Sat)"
+    var day2 = document.getElementById("day2");
+    day2.innerHTML = "Day 2 - January 15-16, 2022 (Sat-Sun)"
+}
+
